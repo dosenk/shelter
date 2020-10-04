8bce3b4ca1d8f99323b63f401e4043218e924516
Dmitry Osipenko <eympel@gmail.com>
2020-10-04 22:15:19 +0300

    feat: start task, add webpack config

---
+++ pages/main/main.js
@@ -0,0 +1,4 @@
+console.log(sdfsdf);
+console.log(sdfsdf);
+console.log(sdfsdf);
+console.log(sdfsdf);
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: './pages/main/main.js',
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist')
+    }, 
+    devServer: {
+        port: 4200
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './pages/index.html'
+        }),
+        new CleanWebpackPlugin()
+    ]
+}
