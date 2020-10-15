72de91befc1c3db0cf3720299d37cf1dd051d502
Dmitry Osipenko <eympel@gmail.com>
2020-10-15 11:51:46 +0300

    refactor: added shelter folder for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     },
     output: {
         filename: '[name].[contenthash].js',
-        path: isDev ? path.resolve(__dirname, 'dist') : path.resolve(__dirname, 'prod')
+        path: isDev ? path.resolve(__dirname, 'dist') : path.resolve(__dirname, 'shelter')
     }, 
     devServer: {
         port: 4200
